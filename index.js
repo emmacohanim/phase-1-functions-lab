13640c816b141e140f4f9571b248cd124048cd5d
Emma Cohanim <emmacohanim@gmail.com>
2022-07-19 15:30:33 -0700

    commit tree 4c6c983f602d58f1eb20fa1d8f09608668398bd8

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(blocks) {
+    let distance
+    if (blocks <= 42) {
+        distance = 42 - blocks
+    } else {
+        distance = blocks - 42
+    }
+    return distance
+}
+
+function distanceFromHqInFeet(blocks) {
+    blocks = distanceFromHqInBlocks(blocks)
+    return blocks * 264
+}
+
+function distanceTravelledInFeet(start, destination) {
+let travel
+if (start >= destination) {
+    travel = (start - destination) * 264
+} else {
+    travel = (destination - start) * 264
+}
+return travel
+}
+
+function calculatesFarePrice(start, destination) {
+    let price
+    let distance = distanceTravelledInFeet(start, destination)
+    if (distance <= 400) {
+      price = 0
+    } else if (distance <= 2000) {
+      price = .02 * (distance - 400)
+    } else if (distance > 2000 && distance <= 2500) {
+      price = 25
+    } else if (distance > 2500) {
+      price = 'cannot travel that far'
+    }
+    return price
+}
